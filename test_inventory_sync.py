8caad30b37fc88d3912cc47306576ef09f7e4f16
Banty <banty.khuraijam@sunbirdtrust.com>
2026-09-02 15:06:10 +0530

    Revert "Merge pull request #3 from banty-kh/fix-persisting-error-issue"
    This reverts commit 04808a78181eb3dd9e62eb4731366c95a9141f41, reversing
    changes made to a2fb54e752c37bbf6c6789b52d2c9428c6e64445.

--- test_inventory_sync.py
+++ test_inventory_sync.py
@@ -25,24 +25,6 @@ class SpreadsheetToDataTests(unittest.TestCase):
         self.assertEqual(data["items"]["Blankets"][0]["year"], "2026")
         self.assertEqual(data["items"]["Blankets"][0]["closing_total"], 12)
 
-    def test_accepts_months_grouped_across_columns(self):
-        rows = [
-            ["Inventory register"],
-            ["Address", "POC", "January 2026", "", "", "February 2026", "", ""],
-            ["", "", "New", "Used", "Total", "New", "Used", "Total"],
-            ["Aben", "Pratip", 3, 2, 5, 4, 2, 6],
-        ]
-        workbook = BytesIO()
-        with pd.ExcelWriter(workbook, engine="openpyxl") as writer:
-            pd.DataFrame(rows).to_excel(writer, sheet_name="Blankets", header=False, index=False)
-
-        data = spreadsheet_to_data(workbook.getvalue())
-
-        january, february = data["items"]["Blankets"]
-        self.assertEqual((january["month"], january["year"]), ("January", "2026"))
-        self.assertEqual((january["closing_new"], january["closing_used"], january["closing_total"]), (3, 2, 5))
-        self.assertEqual((february["month"], february["closing_total"]), ("February", 6))
-
 
 if __name__ == "__main__":
     unittest.main()
